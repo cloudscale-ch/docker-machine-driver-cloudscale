5ba36da0d4a0fd85d0d584c9b6c09a9092a65466
Alain Kaeslin <alain@cloudscale.ch>
2019-05-22 08:14:59 +0200

    Use alakae/cloudscale-go-sdk.

--- driver.go
+++ driver.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"time"
 
-	cloudscale "github.com/cloudscale-ch/cloudscale-go-sdk"
+	cloudscale "github.com/alakae/cloudscale-go-sdk"
 	"github.com/docker/machine/libmachine/drivers"
 	"github.com/docker/machine/libmachine/log"
 	"github.com/docker/machine/libmachine/mcnflag"
